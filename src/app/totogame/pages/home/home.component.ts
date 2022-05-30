4713bb4b14d83f58a81f292efb5197ca54629ca9
André Rosa <joao63430@gmail.com>
2022-05-30 09:11:21 +0200

    fonctionnel home

--- src/app/totogame/pages/home/home.component.ts
+++ src/app/totogame/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TotogameAPI} from "../../API/totogameAPI";
 import {RequestApiPaginate} from "../../interface/RequestApiPaginate";
 import {HttpClientService} from "../../../../services/http-client.service";
@@ -11,74 +11,99 @@ import {sprintf} from "sprintf-js";
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  private _iButton: number = 0
+  limitApi: boolean = false
+  private _totalApiResponse: number = 9;
+  gamesAlpha: Game[] = [];
+  gamesPrices: Game[] = [];
+  gamesPublishedAt: Game[] = [];
+  gamesFilter: GamesList[] = [];
 
-  games: Game[] = [];
-  private _limitApi:number =9
-  private _totalApiResponse:number =0
+  constructor(private httpService: HttpClientService) {
+  }
 
-  constructor(private httpService: HttpClientService) { }
 
-  limitationApi(value?:number):string{
-    if (value !==0){
-      return "?limit="+value
-    } else {
-      return ""
+  buttonsAllGameFunction(id:number){
+    this.limitApi = !this.limitApi
+    this.gamesFilter = [];
+    console.log(id)
+    console.log(this)
+    console.log(event)
+    if ( id === 1){
+      this.apiLoadPublishedAt()
+    } else if ( id === 2){
+      this.apiLoadPrice()
+    } else if (id === 3){
+      this.apiLoadName()
     }
   }
 
-  allGame(){
-    if (this._limitApi !==this._totalApiResponse){
-      this._limitApi = this._totalApiResponse
-    } else {
-      this._limitApi = 9
-    }
-
-    this.loadAPI()
+  apiLoad(){
+    this.gamesFilter = [];
+    this.limitApi = !this.limitApi
+    this.apiLoadPublishedAt()
+    this.apiLoadPrice()
+    this.apiLoadName()
+    console.log(this.gamesFilter)
+    this.gamesFilter.sort(function compare(a, b) {
+      if (a.id < b.id)
+        return -1;
+      if (a.id > b.id )
+        return 1;
+      return 0;
+    });
+    console.log(this.gamesFilter)
   }
 
-  loadAPI(){
-    this.httpService.getRequest<IApiResponse<Game>>(sprintf(TotogameAPI.urlGameFilter,'publishedAt', 'desc', 9)).subscribe((json) => {
+
+
+  apiLimit(): number {
+    if (!this.limitApi) {
+      return this._totalApiResponse
+    } else {
+      return 9
+    }
+  }
+
+  apiLoadPublishedAt() {
+    this.httpService.getRequest<RequestApiPaginate<Game>>(sprintf(TotogameAPI.urlGameFilter, 'publishedAt', 'desc', this.apiLimit())).subscribe((json) => {
       this.gamesPublishedAt = json.items;
+      this._totalApiResponse = json.total
+      this.gamesFilter.push({games: this.gamesPublishedAt, name: 'Les dernières sorties', id: 1});
     });
-    this.httpService.getRequest<IApiResponse<Game>>(sprintf(TotogameAPI.urlGameFilter,'price', 'desc', 9)).subscribe((json) => {
+  }
+
+  apiLoadPrice() {
+    this.httpService.getRequest<RequestApiPaginate<Game>>(sprintf(TotogameAPI.urlGameFilter, 'price', 'desc', this.apiLimit())).subscribe((json) => {
       this.gamesPrices = json.items;
+      this.gamesFilter.push({games: this.gamesPrices, name: 'Les meilleures ventes', id: 2});
     });
-    this.httpService.getRequest<IApiResponse<Game>>(sprintf(TotogameAPI.urlGameFilter,'name', 'asc', 9)).subscribe((json) => {
+  }
+
+  apiLoadName() {
+    this.httpService.getRequest<RequestApiPaginate<Game>>(sprintf(TotogameAPI.urlGameFilter, 'name', 'asc', this.apiLimit())).subscribe((json) => {
       this.gamesAlpha = json.items;
-    });
-    this.httpService.getRequest<RequestApiPaginate<Game>>(TotogameAPI.url("/game"+this.limitationApi(this._limitApi))).subscribe((json ) => {
-      json.items.filter( item => {
-        json.limit = 1;
-        json.page = 1;
-        json.itemsPerPage = 9;
-        this._totalApiResponse = json.total;
-      })
-      this.games = json.items
-      console.log(this.games)
+      this.gamesFilter.push({games: this.gamesAlpha, name: 'Les jeux', id: 3});
     });
   }
 
 
   ngOnInit(): void {
-
-    this.loadAPI()
-
+    this.apiLoad()
   }
 
 
-  get limitApi(): number {
-    return this._limitApi;
+  iButton(): number {
+    let i = this._iButton
+    this._iButton++;
+    return i
   }
 
-  set limitApi(value: number) {
-    this._limitApi = value;
-  }
-
-  get totalApiResponse(): number {
-    return this._totalApiResponse;
-  }
-
-  set totalApiResponse(value: number) {
-    this._totalApiResponse = value;
-  }
 }
+
+interface GamesList {
+  games: Game[];
+  name: string;
+  id: number
+}
+
